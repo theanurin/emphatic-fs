6b783938e36ab98e895a05af644de5cfb5320993
Matt Signorini <matthew.signorini@gmail.com>
2013-06-20 18:27:08 +1000

    Implemented get_fat_entry and put_fat_entry in table.c, and finished implementing the cache management functions. I have decided not to use write allocate, as per the previous commit, because it would create unnecessary IO operations. Instead, writes to FAT entries bypass the cache entirely.
    Write allocate is only really sensible in a cache that uses write back
    (we use write through), which is not at all a good idea for our
    application: If the FAT sector cache is only flushed on umount, then in the
    event of (say) a power blackout, users would lose all their work, even
    though they saved regularly, because the newly allocated clusters are only
    recorded in the cache (which is lost in a power outage), not on the actual
    storage device.

--- table.c
+++ table.c
@@ -48,6 +48,9 @@ PRIVATE void add_to_mru (cache_entry_t *new_item);
 PRIVATE cache_entry_t * unlink_item (cache_entry_t **item_pointer);
 PRIVATE bool lookup_key (cache_entry_t **found, unsigned int key);
 
+// function to fetch a given FAT sector from the cache.
+PRIVATE fat_entry_t * get_sector (unsigned int index);
+
 
 // global pointer to the volume information for the file system that we
 // have mounted.
@@ -70,6 +73,62 @@ table_init (v)
     // initialise the cache structure's fields.
     cache.head = cache.tail = NULL;
     cache.available = CACHE_SECTORS_MAX;
+}
+
+/**
+ *  Read the contents of a given cell in the FAT. Return value is the cell
+ *  contents.
+ */
+    PUBLIC fat_entry_t
+get_fat_entry (entry)
+    fat_entry_t entry;      // index of the cell to read.
+{
+    fat_entry_t *sector;
+    unsigned int fat_offset, sector_index;
+
+    // get the offset of the required FAT entry in bytes.
+    fat_offset = entry * FAT_ENTSIZE;
+
+    // get the index of the sector that contains that entry.
+    sector_index = fat_offset / SECTOR_SIZE (volume_info);
+
+    // fetch the sector.
+    sector = get_sector (sector_index);
+
+    // calculate the offset of the entry into the sector.
+    fat_offset = (fat_offset % SECTOR_SIZE (volume_info)) / 
+        sizeof (fat_entry_t);
+
+    // return the FAT entry from within the sector.
+    return sector [fat_offset];
+}
+
+/**
+ *  Write a new value to a particular entry in the FAT. This procedure
+ *  uses "no write allocate", ie. if the FAT sector being written to is
+ *  not present in the cache, it will not be brought in.
+ */
+    PUBLIC void
+put_fat_entry (entry, val)
+    fat_entry_t entry;          // index of FAT entry to write to.
+    fat_entry_t val;            // value to write there.
+{
+    unsigned int offset, index;
+    size_t sector_size = SECTOR_SIZE (volume_info);
+
+    // calculate sector index, and offset within that sector.
+    offset = entry * FAT_ENTSIZE;
+    index = offset / sector_size;
+    offset %= sector_size;
+
+    // seek the device file to the correct byte offset of the FAT entry
+    // that is to be written to.
+    safe_seek (volume_info->dev_fd, 
+      (FAT_START (volume_info) + index) * sector_size + offset,
+      SEEK_SET);
+
+    // write in the new value.
+    safe_write (volume_info->dev_fd, &val, sizeof (fat_entry_t));
 }
 
 /**
@@ -116,10 +175,84 @@ unlink_item (item_pointer)
 {
     cache_entry_t *temp = *item_pointer;
 
-    // unlink the item.
+    // unlink the item. Remember to increment the number of available
+    // cache slots, because one slot becomes free as a result of unlinking.
     *item_pointer = (*item_pointer)->next;
+    cache.available += 1;
 
     return temp;
+}
+
+/**
+ *  Search for a cache item that matches a given key. If found, that item
+ *  will be unlinked from the cache item list, and added to the MRU end,
+ *  and the variable pointed to by the found parameter will be set to
+ *  point to the cache item. 
+ *
+ *  Return value is true if an item was found, or false if not.
+ */
+    PRIVATE bool
+lookup_item (found, key)
+    cache_entry_t **found;      // address of var to point to found item.
+    unsigned int key;           // key to match to.
+{
+    cache_entry_t **cp;
+
+    // step through the cache from the start of the list (LRU end) until
+    // we either find a matching item, or reach the end of the list.
+    for (cp = &(cache.lru); ((*cp)->key != key) && (*cp != NULL);
+      cp = &((*cp)->next))
+    {
+        ;
+    }
+
+    // have we found a match for the key?
+    if (cp != NULL)
+    {
+        // yes. Point *found to the matching item, move the item to the
+        // MRU end of the list and return true.
+        *found = *cp;
+        add_to_mru (unlink_item (cp));
+        return true;
+    }
+
+    // if we reach this point, we did not find a match. Return false.
+    return false;
+}
+
+/**
+ *  Fetch a given sector from within the FAT.
+ *
+ *  Return value is a pointer to a buffer of size one sector containing
+ *  the contents of the FAT sector.
+ */
+    PRIVATE fat_entry_t *
+get_sector (index)
+    unsigned int index;         // sector index, from start of FAT.
+{
+    cache_entry_t *cache_item;
+
+    // first try looking up the item in the cache. Is it already there?
+    if (lookup_item (&cache_item, index) == false)
+    {
+        // not found, so we will need to read the FAT sector in and add it
+        // to the cache.
+        cache_item = safe_malloc (sizeof (cache_entry_t));
+        cache_item->key = index;
+        cache_item->sector = safe_malloc (SECTOR_SIZE (volume_info));
+
+        // seek to and read in the FAT sector.
+        safe_seek (volume_info->dev_fd,
+          (FAT_START (volume_info) + index) * SECTOR_SIZE (volume_info),
+          SEEK_SET);
+        safe_read (volume_info->dev_fd, cache_item->sector, 
+          SECTOR_SIZE (volume_info));
+
+        // add the new cache item to the MRU end of the cache list.
+        add_to_mru (cache_item);
+    }
+
+    return cache_item->sector;
 }
 
 
